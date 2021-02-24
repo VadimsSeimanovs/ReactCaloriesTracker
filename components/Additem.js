dbe8b18a4535a9078923aff0ec6fce0b8ec3e592
Elena Secara <elena.alexandra189@gmail.com>
2021-02-24 17:51:23 +0000

    wip(AddItem)

--- components/Additem.js
+++ components/Additem.js
@@ -4,12 +4,31 @@ import { View } from 'react-native';
 import { SafeAreaView, ScrollView, } from 'react-native';
 import { Input, Button, Text } from 'react-native-elements'
 import styles  from './styles/button'
+import UserProvider from './Firebase'
 
 export default class AddItem extends React.Component {
+    state = {
+        itemBarcode: '000',
+        itemName: '',
+        itemEnergy: '',
+        itemFat: '',
+        itemCarbs: '',
+        itemSugar: '',
+        itemFiber: '',
+        itemProtein: '',
+        itemSalt: ''
+    }
+
+    onChangeText = (key, val) => {
+        this.setState({ [key]: val })
+      }
 
   checkUserInput = () => {
-   
-  }
+    UserProvider.init()
+    UserProvider.insertItem(this.state.itemBarcode, this.state.itemName, this.state.itemEnergy, this.state.itemFat, this.state.itemCarbs, this.state.itemSugar, this.state.itemProtein, this.state.itemSalt)
+    UserProvider.getItem()
+    console.log("state "+ state.itemName)  
+}
 
   render(){
     return (
@@ -19,72 +38,90 @@ export default class AddItem extends React.Component {
         <Input style={styles.textInput}
             placeholderTextColor='#707070'
             placeholder='Item name'
+            onChangeText={val=> this.onChangeText('itemName', val)}
+            // value={this.state.itemName}
         />
 
-        <Input style={styles.textInput}
+        {/* <Input style={styles.textInput}
             placeholderTextColor='#707070'
             placeholder='Brand'
-        />
+            value={this.state.bran}
+        /> */}
 
-        <Input style={styles.textInput}
+        {/* <Input style={styles.textInput}
             placeholderTextColor='#707070'
             placeholder='Serving size'
-        />
+        /> */}
 
         <Input style={styles.textInput}
             placeholderTextColor='#707070'
             placeholder='Calories'
+            onChangeText={val=> this.onChangeText('itemEnergy', val)}
+            // value={this.state.itemEnergy}
         />
 
         <Input style={styles.textInput}
             placeholderTextColor='#707070'
             placeholder='Fat'
+            // onChangeText={val=> this.onChangeText('itemFat', val)}
+            // value={this.state.itemFat}
         />
 
-        <Input style={styles.textInput}
+        {/* <Input style={styles.textInput}
             placeholderTextColor='#707070'
             placeholder='Saturated fat'
-        />
+        /> */}
 
-        <Input style={styles.textInput}
+        {/* <Input style={styles.textInput}
             placeholderTextColor='#707070'
             placeholder='Cholesterol'
-        />
+        /> */}
 
         <Input style={styles.textInput}
             placeholderTextColor='#707070'
             placeholder='Sodium'
+            // onChangeText={val=> this.onChangeText('itemSalt', val)}
+            // value={this.state.itemSalt}
         />
 
         <Input style={styles.textInput}
             placeholderTextColor='#707070'
             placeholder='Carbohydrates'
+            // onChangeText={val=> this.onChangeText('itemCarbs', val)}
+            // value={this.state.itemCarbs}
         />
 
         <Input style={styles.textInput}
             placeholderTextColor='#707070'
             placeholder='Fibre'
+            // onChangeText={val=> this.onChangeText('itemFiber', val)}
+            // value={this.state.itemFiber}
         />
 
         <Input style={styles.textInput}
             placeholderTextColor='#707070'
             placeholder='Sugar'
+            // onChangeText={val=> this.onChangeText('itemSugar', val)}
+            // value={this.state.itemSugar}
         />
 
         <Input style={styles.textInput}
             placeholderTextColor='#707070'
             placeholder='Protein'
+            // onChangeText={val=> this.onChangeText('itemProtein', val)}
+            // value={this.state.itemProtein}
         />
 
         <Button title='Add' raised onPress = { 
-            () => this.props.navigation.navigate('Dashboard')} 
+            () => this.checkUserInput()}
+            // () => this.props.navigation.navigate('Dashboard')} 
             buttonStyle={{backgroundColor: '#0F2080'}}
         />
 
         <View style={styles.spaceBetweenButtons}></View>
 
         <Button title='Cancel' raised onPress = { 
-            () => this.props.navigation.navigate('Dashboard')} 
+            () => this.props.navigation.navigate('BarcodeScanner')} 
             buttonStyle={{backgroundColor: '#0F2080'}}
         />
           <StatusBar style="auto" />
